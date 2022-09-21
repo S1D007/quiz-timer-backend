838f6e59d4712dc520d0b729fcbb91d901f5b6fc
Siddhant <s1d.official011001011@gmail.com>
2022-09-21 05:44:56 +0530

    Added Models and Connected DAtabase && Fixed Some Issues

---
+++ Database/Schema/index.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose")
+
+/* Creating Schema for the Parameter's */
+
+const QuestionSchema = mongoose.Schema({
+    // Here type = Datatype, required = boolean true and false[true = this feild must be present in the req.body() otherwise it will give error, false = it is not necessary to add the corrosponding feild]
+    category:{
+        type:"String",
+        required:true,
+
+    },
+    level:{
+        type:"String",
+        required:true,
+
+    },
+    question:{
+        type:"String",
+        required:true,
+
+    },
+    options:{
+        type:"String",
+        required:true,
+
+    },
+    correctAnswer:{
+        type:"String",
+        required:true,
+
+    },
+    price:{
+        type:"Number",
+        required:true,
+
+    },
+    prize:{
+        type:"Number",
+        required:true,
+
+    },
+})
+
+module.exports = QuestionSchema
